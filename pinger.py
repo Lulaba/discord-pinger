e666312316bb2ef56669479ca60323f0152d4887
thebotsmith <33283769+thebotsmith@users.noreply.github.com>
2018-11-11 23:00:23 +0000

    Create pinger.py

---
+++ pinger.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import requests
+
+requests.packages.urllib3.disable_warnings()
+
+TOKEN = '' #SET YOUR BOT TOKEN HERE 
+
+client = discord.Client()
+
+keywords = ["fuck","shit","hello"] #SET YOUR KEY WORDS HERE
+
+#FUNKO
+@client.event
+async def on_message(message):
+    for keyword in keywords:
+        if message.content == keyword and message.channel.name == "rules": # SET YOUR CHANNEL NAME HERE
+            await client.send_message(message.channel,"@everyone - keyword matched") #SET YOUR MESSAGE HERE YOU WANT TO USE     
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+while True:
+    client.run(TOKEN)
+
